bccf5a26c355702e6b0c53b1127261a9a730eea9
Christine Banlawi <christine.banlawi@gmail.com>
2021-07-13 18:24:20 -0400

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,56 @@
+let postsArray = [];
+const titleInput = document.getElementById("post-title");
+const bodyInput = document.getElementById("post-body");
+const form = document.getElementById("post-form");
+
+function renderPosts() {
+  let postInfo = "";
+
+    postsArray.forEach(post => {
+      postInfo += `
+        <div class="individual-post">     
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+        </div>
+      `;
+    });
+
+    document.querySelector(".post-container").innerHTML = postInfo;
+};
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then(response => response.json())
+  .then(data => {
+    postsArray = data.slice(0, 5);
+
+    renderPosts();
+  });
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const postTitle = titleInput.value;
+    const postBody = bodyInput.value;
+    const postData = {
+      title: postTitle,
+      body: postBody
+    };
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(post => {
+        
+        postsArray.unshift(post);
+        renderPosts();
+
+        // titleInput.value = "";
+        // bodyInput.value = "";
+        form.reset();
+      });
+  });
